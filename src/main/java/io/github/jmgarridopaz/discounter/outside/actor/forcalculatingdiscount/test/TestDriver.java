5fddc924ef568bea9493dd1729a1ce0011a754ed
jmgarridopaz <44113936+jmgarridopaz@users.noreply.github.com>
2022-09-07 11:10:53 +0200

    Update TestDriver.java

--- src/main/java/io/github/jmgarridopaz/discounter/outside/actor/forcalculatingdiscount/test/TestDriver.java
+++ src/main/java/io/github/jmgarridopaz/discounter/outside/actor/forcalculatingdiscount/test/TestDriver.java
@@ -15,4 +15,15 @@ public class TestDriver {
 
 	}
 
+/*
+ private static final String OUTPUT_DIRECTORY = System.getProperty("user.home") + File.separator + ".discounter" + File.separator + "test-output";
+
+    public static void main (String[] args ) {
+        TestNG testNG = new TestNG();
+        testNG.setOutputDirectory(OUTPUT_DIRECTORY);
+        testNG.setTestClasses ( new Class[]{ DiscounterAppTest.class } );
+        testNG.run();
+    }
+*/	
+	
 }
